7711bcdb4dff8093e909959057c3729ee2a6f095
Jonathan Palacio <jonathanpalacio@jonathambpm3max.lan>
2024-02-16 14:29:15 -0500

    added javascript-react and typescriptreact to dap config

--- neovim-new/.config/nvim/lua/custom/configs/dap.lua
+++ neovim-new/.config/nvim/lua/custom/configs/dap.lua
@@ -9,7 +9,7 @@ dap.adapters["pwa-node"] = {
   },
 }
 
-for _, language in ipairs { "typescript", "javascript" } do
+for _, language in ipairs { "typescript", "javascript", "javascriptreact", "typescriptreact" } do
   dap.configurations[language] = {
     {
       type = "pwa-node",
