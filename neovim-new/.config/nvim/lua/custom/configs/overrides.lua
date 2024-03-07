defb790abb42bfb40181be8680212f4be77b11a3
Jonathan Palacio <jonathanpalacio@jonathambpm3max.lan>
2024-03-07 14:42:19 -0500

    added copilot to nvim setup and working / exploring

--- neovim-new/.config/nvim/lua/custom/configs/overrides.lua
+++ neovim-new/.config/nvim/lua/custom/configs/overrides.lua
@@ -1,83 +1,92 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "go",
-    "python",
-    "json",
-    "yaml",
-    "toml",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"go",
+		"python",
+		"json",
+		"yaml",
+		"toml",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "tailwindcss-language-server",
-    "deno",
-    "prettier",
-    "eslint-lsp",
-    "js-debug-adapter",
-    "json-lsp",
-    "yaml-language-server",
-    "jq-lsp",
-    "sqlls",
-    "bash-language-server",
-    "emmet-ls",
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"tailwindcss-language-server",
+		"deno",
+		"prettier",
+		"eslint-lsp",
+		"js-debug-adapter",
+		"json-lsp",
+		"yaml-language-server",
+		"jq-lsp",
+		"sqlls",
+		"bash-language-server",
+		"emmet-ls",
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
 
-    -- GO
-    "gopls",
+		-- GO
+		"gopls",
 
-    -- DOCKER
-    "docker-compose-language-service",
-    "dockerfile-language-server",
+		-- DOCKER
+		"docker-compose-language-service",
+		"dockerfile-language-server",
 
-    -- PYTHON
-    "pyright",
-    "black",
-  },
+		-- PYTHON
+		"pyright",
+		"black",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
+}
+
+M.copilot = {
+	suggestion = {
+		enable = true,
+	},
+	panel = {
+		enable = true,
+	},
 }
 
 return M
--- neovim-new/.config/nvim/lua/custom/mappings.lua
+++ neovim-new/.config/nvim/lua/custom/mappings.lua
@@ -2,104 +2,112 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    -- Toggle Transparency
-    ["<leader>tt"] = {
-      function()
-        require("base46").toggle_transparency()
-      end,
-      "Toggle transparency",
-    },
-    ["<C-e>"] = { "3<C-e>", "Scroll Down Quickly" },
-    ["<C-y>"] = { "3<C-y>", "Scroll Up Quickly" },
-    ["jk"] = { "<esc>", "Exit Insert Mode" },
-    ["<leader><leader>l"] = { ":noh<CR><C-L>", "Remove search highlights" },
-    ["<leader>m"] = { ":bufdo! bw<CR>", "Close all buffers"},
-    ["<C-h>"] = { ":TmuxNavigateLeft<CR>", "Tmux/Nvim Move Left"},
-    ["<C-j>"] = { ":TmuxNavigateDown<CR>", "Tmux/Nvim Move Down"},
-    ["<C-k>"] = { ":TmuxNavigateUp<CR>", "Tmux/Nvim Move Up"},
-    ["<C-l>"] = { ":TmuxNavigateRight<CR>", "Tmux/Nvim Move Right"},
-    ["<C-\\>"] = { ":TmuxNavigatePrevious<CR>", "Tmux/Nvim Move Previous"},
-    ["<leader><leader>f"] = { ":HopAnywhere<CR>", "Hop Anywhere"},
-    ["<leader><leader>fw"] = { ":HopWord<CR>", "Hop Word"},
-    ["<leader><leader>fc"] = { ":HopChar2<CR>", "Hop Character Two or if only one letter hit enter"},
-    ["<leader><leader>fl"] = { ":HopLine<CR>", "Hop Line"},
-    ["<leader><leader>fls"] = { ":HopLineStart<CR>", "Hop Line Start"},
-    ["<leader><leader>fp"] = { ":HopPattern<CR>", "Hop Pattern"},
-    ["<leader><leader>t"] = {
-      function()
-        require("nvterm.terminal").toggle "float"
-      end,
-      "Toggle floating term",
-    },
-
-    --  format with conform
-    ["<leader>fm"] = {
-      function()
-        require("conform").format()
-      end,
-      "formatting",
-    },
-  },
-  v = {
-    [">"] = { ">gv", "indent" },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		-- Toggle Transparency
+		["<leader>tt"] = {
+			function()
+				require("base46").toggle_transparency()
+			end,
+			"Toggle transparency",
+		},
+		["<C-e>"] = { "3<C-e>", "Scroll Down Quickly" },
+		["<C-y>"] = { "3<C-y>", "Scroll Up Quickly" },
+		["jk"] = { "<esc>", "Exit Insert Mode" },
+		["<leader><leader>l"] = { ":noh<CR><C-L>", "Remove search highlights" },
+		["<leader>m"] = { ":bufdo! bw<CR>", "Close all buffers" },
+		["<C-h>"] = { ":TmuxNavigateLeft<CR>", "Tmux/Nvim Move Left" },
+		["<C-j>"] = { ":TmuxNavigateDown<CR>", "Tmux/Nvim Move Down" },
+		["<C-k>"] = { ":TmuxNavigateUp<CR>", "Tmux/Nvim Move Up" },
+		["<C-l>"] = { ":TmuxNavigateRight<CR>", "Tmux/Nvim Move Right" },
+		["<C-\\>"] = { ":TmuxNavigatePrevious<CR>", "Tmux/Nvim Move Previous" },
+		["<leader><leader>f"] = { ":HopAnywhere<CR>", "Hop Anywhere" },
+		["<leader><leader>fw"] = { ":HopWord<CR>", "Hop Word" },
+		["<leader><leader>fc"] = { ":HopChar2<CR>", "Hop Character Two or if only one letter hit enter" },
+		["<leader><leader>fl"] = { ":HopLine<CR>", "Hop Line" },
+		["<leader><leader>fls"] = { ":HopLineStart<CR>", "Hop Line Start" },
+		["<leader><leader>fp"] = { ":HopPattern<CR>", "Hop Pattern" },
+		["<leader><leader>t"] = {
+			function()
+				require("nvterm.terminal").toggle("float")
+			end,
+			"Toggle floating term",
+		},
+		--  format with conform
+		["<leader>fm"] = {
+			function()
+				require("conform").format()
+			end,
+			"formatting",
+		},
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
+	},
+	i = {
+		["<C-l>"] = {
+			function()
+				vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+			end,
+			"Copilot Accept",
+			{ replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
+		},
+	},
 }
 
 -- GO
 M.dap = {
-  plugin = true,
-  n = {
-    ["<leader>db"] = {
-      "<cmd> DapToggleBreakpoint <CR>",
-      "Add breakpoint at line",
-    },
-    ["<leader>dr"] = {
-      "<cmd> DapContinue <CR>",
-      "Run or continue the debugger",
-    },
-    ["<leader>dus"] = {
-      function()
-        local widgets = require "dap.ui.widgets"
-        local sidebar = widgets.sidebar(widgets.scopes)
-        sidebar.open()
-      end,
-      "Open debugging sidebar",
-    },
-  },
+	plugin = true,
+	n = {
+		["<leader>db"] = {
+			"<cmd> DapToggleBreakpoint <CR>",
+			"Add breakpoint at line",
+		},
+		["<leader>dr"] = {
+			"<cmd> DapContinue <CR>",
+			"Run or continue the debugger",
+		},
+		["<leader>dus"] = {
+			function()
+				local widgets = require("dap.ui.widgets")
+				local sidebar = widgets.sidebar(widgets.scopes)
+				sidebar.open()
+			end,
+			"Open debugging sidebar",
+		},
+	},
 }
 
 M.dap_go = {
-  plugin = true,
-  n = {
-    ["<leader>dgt"] = {
-      function()
-        require("dap-go").debug_test()
-      end,
-      "Debug go test",
-    },
-    ["<leader>dgl"] = {
-      function()
-        require("dap-go").debug_last()
-      end,
-      "Debug last go test",
-    },
-  },
+	plugin = true,
+	n = {
+		["<leader>dgt"] = {
+			function()
+				require("dap-go").debug_test()
+			end,
+			"Debug go test",
+		},
+		["<leader>dgl"] = {
+			function()
+				require("dap-go").debug_last()
+			end,
+			"Debug last go test",
+		},
+	},
 }
 
 M.gopher = {
-  plugin = true,
-  n = {
-    ["<leader>gsj"] = {
-      "<cmd> GoTagAdd json <CR>",
-      "Add json struct tags",
-    },
-    ["<leader>gsy"] = {
-      "<cmd> GoTagAdd yaml <CR>",
-      "Add yaml struct tags",
-    },
-  },
+	plugin = true,
+	n = {
+		["<leader>gsj"] = {
+			"<cmd> GoTagAdd json <CR>",
+			"Add json struct tags",
+		},
+		["<leader>gsy"] = {
+			"<cmd> GoTagAdd yaml <CR>",
+			"Add yaml struct tags",
+		},
+	},
 }
 -- END GO
 
